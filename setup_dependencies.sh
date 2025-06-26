704ae7b6024870ab4fca40d7cc5ae9f0be9f5db6
Christos Besiris <127224336+Nyxteridass@users.noreply.github.com>
2025-06-26 22:01:20 +0300

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -5,8 +5,8 @@ sudo apt update && sudo apt upgrade -y
 echo "Installing system dependencies..."
 sudo apt install -y \
     git python3-pip python3-opencv \
-    libatlas-base-dev libjasper-dev \
-    libqtgui4 python3-pyqt5 \
+    libatlas-base-dev libqt5gui5 \
+    python3-pyqt5 \
     libilmbase-dev libopenexr-dev \
     libgstreamer1.0-dev tesseract-ocr
 
