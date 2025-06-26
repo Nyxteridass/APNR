f7da709db568d79b4891d8a67f693535b37979aa
Christos Besiris <127224336+Nyxteridass@users.noreply.github.com>
2025-06-26 22:12:16 +0300

    Update setup_dependencies.sh

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -2,12 +2,22 @@
 echo "Updating system..."
 sudo apt update && sudo apt upgrade -y
 
-echo "Installing system dependencies..."
+echo "Installing system-level dependencies..."
 sudo apt install -y \
-    git python3-pip python3-opencv \
+    python3 python3-pip python3-opencv \
     libatlas-base-dev libqt5gui5 \
     python3-pyqt5 \
     libilmbase-dev libopenexr-dev \
-    libgstreamer1.0-dev tesseract-ocr
+    libgstreamer1.0-dev tesseract-ocr git
 
 echo "System dependencies installed."
+
+# Check if requirements.txt exists
+if [ -f "requirements.txt" ]; then
+    echo "Installing Python packages from requirements.txt..."
+    pip3 install --upgrade pip
+    pip3 install -r requirements.txt
+    echo "Python packages installed."
+else
+    echo "File named : requirements.txt not found. Skipping pip installation."
+fi
